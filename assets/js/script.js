c28553bc22d3399c2ba312df9208d13299ed8211
Caio <bladellano@gmail.com>
2022-01-11 08:25:32 -0300

    Adicionando scroll no menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 $(function () {
 
+    // Scroll page
+
+    var scrollLink = $('.scroll');
+    
+    scrollLink.click(function (e) {
+		if (!$(this.hash).length)/* Retorna ao index se não encontrar ids */
+			return location.href = location.href.match(/^http.*?.\w\//m)[0];
+
+		e.preventDefault();
+
+		$('body,html').animate({//this.hash - pega o valor do atributo id
+			scrollTop: $(this.hash).offset().top
+		}, 220)
+	});
+
     // Owl carousel
     const owl = $('.owl-carousel').owlCarousel({
         loop: true,
