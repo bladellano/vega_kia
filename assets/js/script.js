d5c2a93bbcc5fc886b65a186facc27b35255ec7f
Caio <bladellano@gmail.com>
2022-01-20 17:27:45 -0300

    Iniciando tratamento para mobilet

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,15 +19,8 @@ $(function () {
         sync: "#carousel",
         controlsContainer: $(".custom-controls-container"),
         customDirectionNav: $(".custom-navigation a")
-        
+
     });
-
-/*     $('#slider').flexslider({
-        animation: "slide",
-        controlsContainer: $(".custom-controls-container"),
-        customDirectionNav: $(".custom-navigation a")
-      }); */
-
 
     // Scroll page
 
@@ -44,22 +37,11 @@ $(function () {
         }, 220)
     });
 
-    // Owl carousel
-    const owl = $('.owl-carousel').owlCarousel({
-        loop: true,
-        items: 1,
-        dots: false,
-        autoplay: true,
-    });
-
-    $('.customNextBtn').click(() => owl.trigger('next.owl.carousel'));
-    $('.customPrevBtn').click(() => owl.trigger('prev.owl.carousel'));
-
     //Fixa navbar
     const navbar = document.querySelector('nav.navbar');
     const headerHeight = document.querySelector('.header_info').offsetHeight;
     window.onscroll = () => {
-        if (window.pageYOffset > headerHeight) {
+        if (window.pageYOffset > headerHeight && window.screen.width > 576) {
             navbar.classList.add('navbar-fixed-top');
         } else {
             navbar.classList.remove('navbar-fixed-top');
